bf3cda6b5fa103b507409daad442136a017cbe32
Doug Leasure <doug.leasure@gmail.com>
2020-03-28 08:16:27 +0000

    Update footFun.R

--- R/footFun.R
+++ R/footFun.R
@@ -2,6 +2,7 @@
 #' @description This is an example function that prints a message.
 #' @param txt Text to append to "foot"
 #' @return Prints a message to the R console.
+#' @author Harry Foot
 #' @export
 
 footFun <- function(txt='funk'){
