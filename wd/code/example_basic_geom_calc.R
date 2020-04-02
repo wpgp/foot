8d2ddda85b525770ba7cc2fa47720cd78be8fac5
wcjochem <7605755+wcjochem@users.noreply.github.com>
2020-04-02 17:14:10 +0100

    summarising values with DT

--- wd/code/example_basic_geom_calc.R
+++ wd/code/example_basic_geom_calc.R
@@ -35,7 +35,7 @@ buildings <- lapply(allFiles, function(x){ st_read(x) })
 
 # calculate basic geometry-based measures
 buildings <- lapply(buildings, function(b){
-  b$year <- substr(as.character(b$ImgDate), 1, 4)  # source image date
+  b$year <- as.numeric(substr(as.character(b$ImgDate), 1, 4))  # source image date
   b$area <- st_area(b)  # units set by projection (usually meters)
   b$area_ha <- set_units(b$area, ha)  # convert to hectares
   
@@ -63,9 +63,36 @@ building_pts <- lapply(buildings, function(b){
 rm(buildings); gc()  # not using polygons
 
 # combine into a single file
+# this could be useful to searcg local areas or combine regions
+# or consider doing calculations on each file independently
 # use data.table trick
 # source: https://github.com/r-spatial/sf/issues/798#issuecomment-405171212
 building_pts <- st_as_sf(data.table::rbindlist(building_pts))
   dim(building_pts)  # n= 3706822
   head(building_pts)
 
+building_pts <- data.table(st_drop_geometry(building_pts))
+# remove records that don't cover mastergrid
+setkey(building_pts, cID)
+building_pts <- building_pts[!is.na(cID)]
+
+# summary by mastergrid cell locations
+building_summary <- building_pts[, .(built=1, 
+                                     # year=mean(year), 
+                                     count=.N,
+                                     total_area=sum(area_ha),
+                                     total_perim=sum(perim),
+                                     p_a_ratio=mean(perim / area),  # using m^2
+                                     area_mean=mean(area_ha),
+                                     area_sd=sd(area_ha),
+                                     perim_mean=mean(perim),
+                                     perim_sd=sd(perim)
+                                    ), 
+                                 by=cID]
+# year by mode
+modeYr <- setkeyv(building_pts[, .N, by=.(cID, year)], c("cID","N"))[, .(year=year[.N]), by=cID]
+# calculate group-level variations
+building_summary$area_cv <- building_summary$area_sd / building_summary$area_mean
+building_summary$perim_cv <- building_summary$perim_sd / building_summary$perim_mean
+  building_summary
+  
