a6e87aa5f964f35827251731cf40314841a7b49a
Chris Jochem <wcjochem@users.noreply.github.com>
2020-04-29 09:36:01 +0100

    improved NN dist

--- R/get_distance.R
+++ R/get_distance.R
@@ -14,67 +14,80 @@ suggestUTMzone <- function(pt){
 }
 
 #' @title Nearest neighbour distance calculation
-#' 
-#' @description Helper function to provide a distance calculation between spatial
-#' ojects. The distance to the first nearest neighbour found (optionally within 
-#' a maximum search radisu) is returned.
-#' 
-#' @param X Spatial object, typically polygons or points.
+#'
+#' @description Helper function to provide a distance calculation between
+#'   spatial ojects. The distance to the first nearest neighbour found
+#'   (optionally within a maximum search radisu) is returned.
+#'
+#' @param X Spatial object of \code{sf} type, typically polygons or points.
 #' @param Y (Optional) Spatial object to measure distances to.
-#' @param maxSearch Maximum search radius around \code{X} to search. Distance in meters. 
-#' Default is 1000.
-#' @param unit Character abbreviation for the units to return from the distance calculation.
-#' 
-#' @details If \code{Y} is omitted the nearest neighbour distances are found within \code{X}. 
-#' Otherwise, the distance for each object in \code{X} to its nearest neighbour in \code{Y} is
-#' returned. Providing a maximum search radius is strongly advised to speed up the calcluation.
-#' 
+#' @param maxSearch Maximum search radius around \code{X} to search. Distance in
+#'   meters. Default is 1000.
+#' @param unit Character abbreviation for the units to return from the distance
+#'   calculation.
+#'
+#' @details If \code{Y} is omitted the nearest neighbour distances are found
+#'   within \code{X}. Otherwise, the distance for each object in \code{X} to its
+#'   nearest neighbour in \code{Y} is returned. Providing a maximum search
+#'   radius is strongly advised to speed up the calcluation.
+#'
 #' @author Chris Jochem
 #' 
 #' @name fs_NNdist
 #' @export
-fs_NNdist <- function(X, Y, maxSearch=1000, unit="m"){
+fs_NNdist <- function(X, Y, maxSearch=100, unit="m"){
+  # row index
+  uid <- 1:nrow(X)
   
+  if(missing(Y)){
+    searchObj <- X
+  } else{
+    searchObj <- Y
+  }
+  
+  if(sf::st_crs(X) != sf::st_crs(searchObj)){
+    stop("Coordinate reference systems do not match for zone indexing.")
+  }
+  
+  # initial search of intersecting objects (distance = 0)
+  intersects1 <- sf::st_intersects(X, searchObj)
+  DT <- data.table::data.table(uid=uid, 
+                               intersects=lapply(intersects1, function(i) uid[i]))
+  
+  DT[which(lengths(intersects)>1), dist := 0]
+  data.table::setindex(DT, uid)
+  
+  # buffer search
   if(!is.null(maxSearch) & maxSearch > 0){
-    if(missing(Y)){
-      searchBuffer <- X
-    } else{
-      searchBuffer <- Y
-    }
-    
-    if(sf::st_crs(X) != sf::st_crs(searchBuffer)){
-      stop("Coordinate reference systems do not match for zone indexing.")
-    }
-    
-    if(sf::st_is_longlat(searchBuffer)){
+    if(sf::st_is_longlat(searchObj)){
       zn <- suggestUTMzone(sf::st_coordinates(sf::st_centroid(sf::st_as_sfc(sf::st_bbox(X)))))
       
-      searchBuffer <- sf::st_transform(searchBuffer, zn)
+      searchBuffer <- sf::st_transform(searchObj, zn)
       searchBuffer <- sf::st_buffer(searchBuffer, maxSearch)
       searchBuffer <- sf::st_transform(searchBuffer, sf::st_crs(X))
     } else{
-      searchBuffer <- sf::st_buffer(searchBuffer, maxSearch)
+      searchBuffer <- sf::st_buffer(searchObj, maxSearch)
     }
     # get objects within search buffer
-    intersects <- sf::st_intersects(searchBuffer, X)
+    intersects2 <- sf::st_intersects(searchBuffer, X)
     # restricted distance calculation
-    distList <- lapply(seq_along(1:nrow(X)),
-                                     FUN=function(i){ sort(sf::st_distance(X[i,], 
-                                                                           X[intersects[[i]],]))[2] })
+    DT[, intersects := lapply(intersects2, function(i) uid[i])]
     
-  } else{
-    # unrestricted distance search (slower)
-    distList <- lapply(seq_along(1:nrow(X)), 
-                          FUN=function(i){ sort(sf::st_distance(X[i,], X[-i,]))[1] })
+    DT[is.na(dist), 
+       dist := sort(sf::st_distance(X$geometry[uid], X$geometry[unlist(intersects)]))[2], #, tolerance=1
+       by = uid]
+
+  } else{  # unrestricted distance search (slower)
+    DT[is.na(dist), 
+       dist := sort(sf::st_distance(X$geometry[uid], X$geometry[unlist(intersects)]))[2],
+       by = uid]
   }
-  
-  result <- do.call(c, distList)
-  
+
   if(!is.null(unit)){
-    units::set_units(result, unit, mode="standard")
+    DT[, dist := units::set_units(dist, unit, mode="standard")]
   }
   
-  return(result)
+  return(DT[["dist"]])  # return as vector
 }
 
 
