2e8c261ecbe511e57a1cb40783d7a66a673b86f3
Chris Jochem <wcjochem@users.noreply.github.com>
2020-05-15 12:11:42 +0100

    stand-alone shape metrics

--- R/fs_compact_mean.R
+++ R/fs_compact_mean.R
@@ -4,7 +4,8 @@
 #'   polygons within zones.
 #' @inheritParams fs_area_mean
 #' @return \code{data.table} of zonal indices and values
-#'
+#' @author Chris Jochem
+#' 
 #' @details The compactness measure is the Polsby-Popper test. This is
 #'   summarised as the mean for all footprints in the zone identified by
 #'   \code{index}.
@@ -22,7 +23,10 @@
 #' @rdname fs_compact_mean
 #' 
 #' @export 
-fs_compact_mean <- function(X, index=NULL, unit=NULL, col=NULL) UseMethod("fs_compact_mean")
+fs_compact_mean <- function(X, 
+                            index=NULL, 
+                            unit=NULL, 
+                            col=NULL) UseMethod("fs_compact_mean")
 
 
 #' @name fs_compact_mean
@@ -39,14 +43,14 @@ fs_compact_mean.sp <- function(X, index=NULL, unit=NULL, col=NULL){
 #' @export
 fs_compact_mean.sf <- function(X, index=NULL, unit=NULL, col=NULL){
   if(!is.null(col)){
-    warning("Ignoring supplied column.")
-    # if(!col %in% names(X)){
-    #   message("Error: column name not found.")
-    #   stop()
-    # } else{
-    #   names(X)[which(names(X)==col)] <- "fs_area"
-    #   result <- fs_compact_mean_calc(X, index, unit)
-    # }
+    # warning("Ignoring supplied column.")
+    if(!col %in% names(X)){
+      message("Error: column name not found.")
+      stop()
+    } else{
+      names(X)[which(names(X)==col)] <- "fs_compact"
+      result <- fs_compact_mean_calc(X, index, unit)
+    }
   } else{  # column not supplied
     if(any(!sf::st_geometry_type(X) %in% c("POLYGON", "MULTIPOLYGON") )){
       message("Area requires polygon shapes.")
@@ -61,10 +65,8 @@ fs_compact_mean.sf <- function(X, index=NULL, unit=NULL, col=NULL){
         unit <- "m^2"
       }
     }
-    
-    X[["fs_area"]] <- fs_area(X, unit)
-    X[["fs_perimeter"]] <- fs_perimeter(X, "m")
-    
+
+    X[["fs_compact"]] <- fs_compact(X)
     result <- fs_compact_mean_calc(X, index, unit)
   }
   return(result)
@@ -72,18 +74,22 @@ fs_compact_mean.sf <- function(X, index=NULL, unit=NULL, col=NULL){
 
 
 fs_compact_mean_calc <- function(X, index, unit=NULL){
-  if(!"fs_area" %in% names(X)){
-    X[["fs_area"]] <- fs_area(X, "m^2")
-  } else{
-    units(X[["fs_area"]]) <- "m^2"
-  }
-  
-  if(!"fs_area" %in% names(X)){
-    X[["fs_perimeter"]] <- fs_perimeter(X, "m")
-  } else{
-    units(X[["fs_perimeter"]]) <- "m"
-  }
-  
+  if(!"fs_compact" %in% names(X)){
+    if(!"fs_area" %in% names(X)){
+      X[["fs_area"]] <- fs_area(X, "m^2")
+    } else{
+      units(X[["fs_area"]]) <- "m^2"
+    }
+    
+    if(!"fs_perimeter" %in% names(X)){
+      X[["fs_perimeter"]] <- fs_perimeter(X, "m")
+    } else{
+      units(X[["fs_perimeter"]]) <- "m"
+    }
+    
+    X[["fs_compact"]] <- fs_compact(X)
+  } 
+    
   if(is.null(index)){
     warning("No index found, treating as one group.")
     index <- rep(1, nrow(X))
@@ -99,15 +105,11 @@ fs_compact_mean_calc <- function(X, index, unit=NULL){
     }
   } 
   
-  DT <- data.table::data.table(index=index,
-                               a=X[["fs_area"]],
-                               p=X[["fs_perimeter"]])
-  
-  data.table::setkey(DT, index)
-  DT[, compact := (4 * pi * a) / (p^2)]
-  units(DT$compact) <- NULL
-  
   colNam <- "fs_compact_mean"
-  result <- DT[, setNames(.(mean(compact)), colNam), by=index]
+  DT <- data.table::data.table(index=index, 
+                               area_calc=X[["fs_compact"]])
+  data.table::setkey(DT, index)
+  result <- DT[, setNames(.(mean(area_calc)), colNam), by=index]
+  
   return(result)
 }
