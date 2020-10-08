a2be601195358e0ca35eccf1966a906e2cd17945
Chris Jochem <wcjochem@users.noreply.github.com>
2020-10-08 11:37:44 +0100

    clean up description

--- R/foot.R
+++ R/foot.R
@@ -1,21 +1,27 @@
-#' foot: A package for calculating building footprint shape metrics
+#' foot: An R package for calculating building footprint shape metrics
 #' 
-#' The foot package provides functions to calculate summary statistics
-#' of geometric measurements of building footprint polygons. Footprints
+#' The \code{foot} package provides functions to calculate summary statistics
+#' of geometric measurements of building footprint polygons. Footprint
 #' shapes representing buildings are becoming more widely available by
-#' being detected and extracted from very high resolution satellite. Such
+#' being detected and extracted from very high resolution satellite imagery. Such
 #' datasets are spatially detailed but often are unlabelled. However,
 #' the size, shape, and distribution of buildings can suggest possible 
-#' land uses or differences in structure use, socieconomic status, etc.
+#' land uses or differences in structure use, socio-economic status, etc.
+#' 
+#' The \code{foot} package is designed to provide a set of consistent and flexible
+#' tools for processing 2D vector representations of buildings. The
+#' functionality includes basic geometry and morphology measures, distance and
+#' clustering metrics. These calculations are supported with helper functions
+#' for spatial intersections and tiled reading/writing of data.
 #' 
 #' The measurements in \code{foot} include: area, perimeter, nearest-neighbour 
-#' distance, angle of rotation for a bounding rectange, as well as a binary 
+#' distance, angle of rotation for a bounding rectangle, as well as a binary 
 #' indicator of structure presence and a count of structures.
 #' 
-#' These measures can be summarised as a mean, standard deivation, coefficient 
-#' of variation, the nearest neighbour index of clustering, or a (normalised) 
-#' entropy measure for the angles, and can be output as a table or as a 
-#' gridded dataset.
+#' These measures can be summarised as a mean, standard deviation, coefficient
+#' of variation, the nearest neighbour index of clustering, or a (normalised)
+#' entropy measure for the angles. The output can be formatted as a data table
+#' or as a gridded dataset.
 #' 
 #' @section Helper functions:
 #' While each measurement function can be accessed as a standalone function,
@@ -29,7 +35,7 @@
 #' spatial data and providing efficient I/O and parallel processing.
 #' 
 #' @section Credits:
-#' This work was undertaken by members of WorldPop at the University of Southampton.
+#' This work was undertaken by members of the [WorldPop Research Group at the University of Southampton](https://www.worldpop.org/).
 #' 
 #' @docType package
 #' @name foot
