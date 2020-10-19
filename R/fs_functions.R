6fef488c65e918aa1ae7f89caddc05eae7e1fbcc
Chris Jochem <wcjochem@users.noreply.github.com>
2020-10-19 12:42:02 +0100

    Revert "Revert "remove standalone functions""
    This reverts commit f7007b0163369aa2f461e0e28a4024b75eb081f8.

--- R/fs_functions.R
+++ R/fs_functions.R
@@ -19,6 +19,33 @@ count <- function(x){
 }
 
 
-nnindex <- function(x){
+# creating a function factory to use the pre-made function foot::fs_nnindex
+gen_nnindex <- function(z, zoneField=NULL, unit="m"){
+  force(z)
+  force(zoneField)
+  force(unit)
   
+  function(x){
+    if(length(x) == 1){
+      return(0)
+    } else{
+      res <- fs_nnindex(sf::st_as_sf(x), z, zoneField, unit)
+      return(res[["fs_nnindex"]])
+    }
+  }
 }
+
+
+entropy <- function(X){
+  bins <- cut(X, seq(5, 355, 10), labels=F) + 1
+  bins[is.na(bins)] <- 1
+  calc_ent <- -1 * sum(prop.table(table(bins)) * log(prop.table(table(bins))))
+  
+  # normalizing step (see Boeing (2019))
+  hmax <- 3.584
+  hg <- 1.386
+  
+  calc_ent <- 1 - ((calc_ent-hg) / (hmax - hg))^2
+  return(calc_ent)
+}
+
