b6137bdf717db133a4af6c58cc32e09186cdc488
Chris Jochem <wcjochem@users.noreply.github.com>
2020-10-19 13:23:02 +0100

    bring back settled function

--- R/fs_settled_count.R
+++ R/fs_settled_count.R
@@ -1,162 +1,92 @@
+#' Count of footprint locations per zone
+#' 
+#' @description Calculate and summarise selected metrics of building 
+#' footprints within zones.
+#' @param X Spatial object with building footprint polygons
+#' @param index A string identifying a column within \code{X} which provides a
+#'   zonal index for summarising values. Alternatively a vector of indices can
+#'   be provided. If omitted, all observations in \code{X} are assumed to be
+#'   within one zone.
+#' @param unit character or \code{units} object to define area. Default is
+#'   \code{NULL} which will use the units of the spatial reference system
+#' @param col column name within \code{X} with pre-calculated area measures
+#' @return \code{data.table} of zonal indices and values.
+#' 
+#' @import data.table
+#' 
+#' @aliases fs_count
+#' @rdname fs_count
+#' 
+#' @export 
+fs_count <- function(X, index=NULL, col=NULL) UseMethod("fs_count")
 
-footstats <- function(X, index, what='all', how=NULL,
-                      controlZone, controlUnits, controlDist,
-                      verbose=TRUE){
-  # validate values
-  if(is.null(what)) stop("Missing list of characteristics.")
-  if(tolower(what) == 'all'){ getAllMetrics}
-  what <- list(what)
-  # set defaults
-  controlUnits
-  getProjection
-  # create zonal index
-  # precalculate 'what's
-  uchars <- unique(unlist(what))
-  
-  # construct variable names?
-  ncols <- length(what)
-  
-  # expand function list
-  if(!any(sapply(how, is.list)) && ncols > 1) { 
-    how <- replicate(ncols, how, simplify=F)
-  }
-  nfuns <- length(nfuns)
-  stopifnot(ncols == nfuns)
-  # remove "invalid" functions e.g nni only dist; entropy only angle
-  
-  
-  DT <- data.table(mtcars)
-  DT
-  what <- list("mpg","hp")
-  what <- list(list("mpg","hp"), list("gear","carb"))
-  how <- list("mean","min")
-  how <- "mean"
-  how <- list(list("mean"), list("min","sum"))
-  
-  ff <- function(x, y){ sum(1000*x + y) }
-  what <- list(list("mpg","hp"), list(list("am","gear")))
-  how <- list(list("mean"), list("ff"))
-  
-  # DT[, lapply(.SD, function(u){ sapply(how, function(f){ do.call(f, list(u)) }) }), by=cyl, .SDcols=unlist(what)] # not correct
-  
-  # main processing loop
 
-  (results <- lapply(seq_along(what), function(i){
-    c <- what[[i]]
-    print(c)
-    # if(is.list(c)) c <- unlist(c)
-    # if(all(c) == "" | is.null(c)) c <- colnames(X)
-    
-    funcs <- how[[i]]
-    # if(length(fun)==1) fun <- list(fun)
-    
-    rt <- DT[, lapply(funcs, function(f){ do.call(f, unname(.SD)) }), by=cyl, .SDcols=unlist(c)]
-    setnames(rt, c("cyl",paste(c, how, sep="_")))
-  }))
+#' @name fs_count
+#' @export
+fs_count.sp <- function(X, index=NULL, col=NULL){
+  X <- sf::st_as_sf(X)
   
-  
-  # version 31
-  for(i in seq_along(what)){
-    cols <- what[[i]]
-    funcs <- how[[i]]
-    
-    for(c in cols){
-      for(func in funcs){
-        print(DT[, do.call(func, unname(.SD)), by=cyl, .SDcols=c])
-      }
-      # print(DT[, lapply(funcs, function(func){ do.call(func, unname(.SD)) }), by=cyl, .SDcols=c])
+  result <- fs_count(X, index, col)
+  return(result)
+}
+
+
+#' @name fs_count
+#' @export
+fs_count.sf <- function(X, index=NULL, col=NULL){
+  if(!is.null(col)){
+    if(!col %in% names(X)){
+      message("Error: column name not found.")
+      stop()
+    } else{
+        names(X)[which(names(X)==col)] <- "fs_count"
+        result <- fs_count_calc(X, index)
     }
+  } else{
+      X[["fs_count"]] <- 1
+      result <- fs_count_calc(X, index)
+  }
+  return(result)
+}
+
+
+fs_count_calc <- function(X, index){
+  if(!"fs_count" %in% names(X)){
+    X[["fs_count"]] <- 1
   }
   
-  lapply(seq_along(what), function(i){
-    cols <- what[[i]]
-    funcs <- how[[i]]
-    DT[, lapply(.SD, function(c){ sapply(funcs, function(func){ do.call(func, unname(.SD)) })}), by=cyl, .SDcols=unlist(cols)]
-  })
+  indexCol <- "index" # default
   
+  if(is.null(index)){
+    message("No index found, treating as one group.")
+    index <- rep(1, nrow(X))
+  } else{
+    if(is.character(index)){ 
+      if(length(index)==1){ 
+        if(nrow(X)>1){ # it must be a column name
+          if(!index %in% colnames(X)){
+            stop("Index column not found in footprints.")
+          } else{
+            indexCol <- index
+            index <- X[[indexCol]]
+          }
+        } # potential issue if 1 row X and 1 column name - won't affect calcs
+      } else if(length(index != nrow(X))){
+        stop("Invalid length of zonal index.")
+      } 
+    } else if(is.numeric(index)){
+      if(length(index) != nrow(X)){
+        stop("Invalid length of zonal index.")
+      }
+    }
+  } 
   
-  # OK https://stackoverflow.com/questions/29620783/apply-multiple-functions-to-multiple-columns-in-data-table
-  lapply_at <- function(var, funs, ...){
-    results <- sapply(var, function(var){ lapply(funs, do.call, list(var, ...))})
-    names(results) <- vapply(names(var), paste, funs, sep="_", FUN.VALUE=character(length(funs)), USE.NAMES=F)
-    results
-  }
+  colNam <- "fs_count"
+  DT <- data.table::data.table(idxCol=index, 
+                               area_calc=X[["fs_count"]])
+  data.table::setnames(DT, "idxCol", indexCol)
+  data.table::setkeyv(DT, indexCol)
+  result <- DT[, setNames(.(sum(area_calc)), colNam), by=indexCol]
   
-  f3 <- function(x, prob=0.25){ quantile(x, prob)}
-  DT[, lapply_at(.SD, c("mean","sum", "f3")), by=cyl, .SDcols=c("mpg","hp")]
-  
-  f4 <- function(hp, mpg) { sum(hp/mpg) }
-  # f4 <- Curry(sum(x^carb))
-  DT[, do.call("f4", .SD), by=cyl, .SDcols=c("mpg","hp")]
-  
-  
-  
-  
-  
-  tf <- function(x){ print(is.list(x))}
-  DT[, do.call(tf, list(.SD)), by=cyl, .SDcols=c('mpg', 'hp')]
-  
-  
-  
-  
-  # testing
-  ngroups <- length(what)
-  (results <- lapply(seq_along(ngroups), function(i){
-    cols <- what[[i]]
-    funcs <- how[[i]]
-    
-    DT[, lapply(.SD, function(c){ lapply(funcs, function(f){ do.call(f, list(c)) }) }), by=cyl, .SDcols=unlist(cols)]
-  }))
-  
-  # check/make unique names
+  return(result)
 }
-
-
-how <- list(mean, min, max)
-how <- list(mean, list(min, max))
-
-any(sapply( how, is.list))
-
-funs <- list(how)
-nargs <- length(funs)
-if(nargs==1 && is.list(funs[[1L]])){
-  nargs <- length(funs <- funs[[1L]])
-  # funs <- replicate(length(what), funs[[1L]], simplify=F)
-} 
-
-nargs
-if(nargs != length(what)) stop("arguments do not match")
-funs
-
-# footstats(X, index, what=c("area","shape","perimeter"), how=c(mean, min, sd) )
-# footstats(X, index, what=c(list("area","shape"),"perimeter"), how=list(mean, list(min, sd) ))
-# footstats(X, index, what=c("area","shape","perimeter"), how=list(mean, list(min, sd) ))
-
-what = "area"; how=list("mean","max","total")
-what = c("area","mean")
-what = list("area","shape","perimeter"); how = "mean" # or just mean
-what = list("area","shape","perimeter"); how = list("mean","min","sd","cv")
-what = list(list("area","shape"), list("perimeter")); how = list(list("mean","min"), list("sd","cv"))
-
-is.list(what)
-lw <- list(what)
-length(lw[[1]])==1 && is.list(lw[[1]])
-
-
-# expand.args <- function(...){
-#   dots <- list(...)
-#   max_l <- max(lengths(dots))
-#   lapply(dots, rep, length.out=max_l)
-}
-
-expand.args <- function(...){
-  dots <- list(...)
-  max_l <- max(lengths(what))
-  lapply(dots, rep, length.out=max_l)
-}
-
-
-if(!any(sapply(how, is.list))) how <- replicate(length(what), how, simplify=F)
-nargs <- length(how)
-if(nargs != length(what)) stop("argument do not match")
-how
